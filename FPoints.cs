02db42d96a21272cc136fed76375933b89f34add
Артемий Нерадовский <artemy.nerad@gmail.com>
2021-03-28 16:05:08 +0300

    Update

---
+++ FPoints.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OxyPlot;
+using OxyPlot.Series;
+
+namespace Lab3WPF
+{
+    public partial class FPoints
+    {
+        public FunctionSeries logic(double leftBorder, double step, double rightBorder, double coef)
+        {
+            var serie = new FunctionSeries();
+            for (double i = leftBorder; i <= rightBorder; i += step)
+            {
+                double y = (double)(coef * Math.Cosh(i / coef));
+                serie.Points.Add(new DataPoint(i, y));
+            }
+            return serie;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -32,7 +32,7 @@ namespace Lab3WPF
             InitializeComponent();
         }
 
-        enum err
+        enum Err
         {
             errCoef = 0,
             errLB,
@@ -46,50 +46,51 @@ namespace Lab3WPF
             double leftBorder = (double)leftBorderT.Value;
             double rightBorder = (double)rightBorderT.Value;
             double step = (double)stepT.Value;
-            if (coef == 0 || leftBorder >= rightBorder || (leftBorder == 0 && rightBorder == 0) || step == 0)
+            if (coef == 0 || leftBorder >= rightBorder || (leftBorder == 0 && rightBorder == 0) || step <= 0)
             {
                 int error = 0;
                 if (coef == 0)
-                    error = (int)err.errCoef;
+                    error = (int)Err.errCoef;
                 else if (leftBorder == 0 && rightBorder == 0)
-                    error = (int)err.errBB;
+                    error = (int)Err.errBB;
                 else if (leftBorder >= rightBorder)
-                    error = (int)err.errLB;
-                else if (step == 0)
-                    error = (int)err.errStep;
+                    error = (int)Err.errLB;
+                else if (step <= 0)
+                    error = (int)Err.errStep;
                 switch (error)
                 {
-                    case (int)err.errCoef:
+                    case (int)Err.errCoef:
                         MessageBox.Show("Коэффициент не может быть равен 0!");
                         break;
-                    case (int)err.errBB:
+                    case (int)Err.errBB:
                         MessageBox.Show("Границы равны 0!");
                         break;
-                    case (int)err.errLB:
+                    case (int)Err.errLB:
                         MessageBox.Show("Левая граница не может быть больше или равняться правой!");
                         break;
-                    case (int)err.errStep:
-                        MessageBox.Show("Шаг не может быть равен 0!");
+                    case (int)Err.errStep:
+                        MessageBox.Show("Шаг не может быть меньше 0!");
                         break;
                 }
             }
             else
             {
                 var model = new PlotModel { Title = "График функции A/2*(e^(x/a)+e^(-x/a))" };
-                var serie = new FunctionSeries();
-                for (double i = leftBorder; i <= rightBorder; i += step)
-                {
-                    double y = (double)(coef * Math.Cosh(i/coef));
-                    serie.Points.Add(new DataPoint(i, y));
-                }
+                FPoints point = new FPoints();
                 model.LegendPosition = LegendPosition.RightBottom;
                 model.LegendPlacement = LegendPlacement.Outside;
                 model.LegendOrientation = LegendOrientation.Horizontal;
+                var serie = point.logic(leftBorder, step, rightBorder, coef);
                 model.Series.Add(serie);
                 var yAxis = new OxyPlot.Axes.LinearAxis();
                 yAxis.AbsoluteMinimum = -100;
                 yAxis.AbsoluteMaximum = 100;
                 var xAxis = new OxyPlot.Axes.LinearAxis { Position = AxisPosition.Bottom };
+                if (coef < 0.30 && coef > -0.30)
+                {
+                    xAxis.AbsoluteMinimum = -5;
+                    xAxis.AbsoluteMaximum = 5;
+                }
                 xAxis.Title = "X";
                 yAxis.Title = "Y";
                 model.Axes.Add(yAxis);
@@ -99,6 +100,7 @@ namespace Lab3WPF
             }
             Console.WriteLine(coef + " " + leftBorder + " " + step + " " + rightBorder);
         }
+        
         private void ImportFromFile(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
@@ -124,6 +126,7 @@ namespace Lab3WPF
         }
         private void SaveInitialData(object sender, RoutedEventArgs e)
         {
+            
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             if (saveFileDialog.ShowDialog() == true)
             {
@@ -132,6 +135,16 @@ namespace Lab3WPF
                 sr.Close();
             }
         }
+        
+        private void SaveGraph(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                Plot.SaveBitmap(saveFileDialog.FileName);
+            }
+        }
+
 
         private void Exit(object sender, RoutedEventArgs e)
         {
